2bbf74a009b1ba43cea076c53bc952869adaf630
Harishkumar01 <38317268+Harishkumar01@users.noreply.github.com>
2020-02-11 04:35:38 +0000

    Update ml.py

--- ml.py
+++ ml.py
@@ -36,6 +36,27 @@ if __name__ == "__main__":
     print(coefficients)
     
     
+   
+import pandas as pd
+import numpy as np
+
+
+if __name__ == "__main__":
+    coefficients = []
+    bostonData = pd.read_csv("BostonHousing.csv")
+
+    x = bostonData.drop('medv',axis = 1).values
+    y = bostonData['medv'].values
+ 
+    x.reshape(-1,1)
+    
+    onesData = np.ones(shape = x.shape[0]).reshape(-1,1)
+    x = np.concatenate((onesData,x),1)
+
+    coefficients = np.linalg.inv(x.transpose().dot(x)).dot(x.transpose()).dot(y)
+    print(coefficients)
+    
+    
   
  # -*- coding: utf-8 -*-
 """
@@ -166,3 +187,37 @@ for i in range(len(test)):
     den1*=O[i][test[i]][1]
 print("Prediction for ", test)
 print('Yes:',num1/(den1+num1))
+
+
+
+
+
+
+
+
+
+
+# load the iris dataset 
+from sklearn.datasets import load_iris 
+iris = load_iris() 
+
+# store the feature matrix (X) and response vector (y) 
+X = iris.data 
+y = iris.target 
+
+# splitting X and y into training and testing sets 
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1) 
+
+# training the model on training set 
+from sklearn.naive_bayes import GaussianNB 
+gnb = GaussianNB() 
+gnb.fit(X_train, y_train) 
+
+# making predictions on the testing set 
+y_pred = gnb.predict(X_test) 
+
+# comparing actual response values (y_test) with predicted response values (y_pred) 
+from sklearn import metrics 
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
+
